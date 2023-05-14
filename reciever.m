733f4b98a95e075aef500b09a09fd04b4286dcf6
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-14 22:29:09 +0300

    remove plot and unused variable

--- reciever.m
+++ reciever.m
@@ -4,10 +4,8 @@
 %return: it returns the final output
 function filtered_output = reciever(output, f_sampling)
     OUTPUT = fftshift(fft(output));
-    f_vec = linspace(-f_sampling/2, f_sampling/2, length(OUTPUT));
     Ns = (length(OUTPUT)/f_sampling) * 3400;
     OUTPUT([1 : round(Ns), (length(OUTPUT) - round(Ns) + 1) : length(OUTPUT)]) = 0;
-    plot(f_vec, OUTPUT);
     out = real(ifft(ifftshift(OUTPUT)));
     filtered_output = out;
 end
