8f33deb8d03d7a436ec715f4f16c3078cbda43ea
Abdelrahman Hesham <106182388+abdelrahman1503@users.noreply.github.com>
2023-05-18 18:05:11 +0300

    Bug fixed
    While calculating the number of samples to be zero, the equation entered assumed that the sampling frequency equals 4 * the cutoff frequecny so but instead teh difference should be considered

--- reciever.m
+++ reciever.m
@@ -5,7 +5,7 @@ function filtered_output = reciever(output, f_sampling)
 %       sampling frequency
 %   return: it returns the final output
 OUTPUT = fftshift(fft(output));
-Ns = (length(OUTPUT)/f_sampling) * 3400;
+Ns = (length(OUTPUT)/f_sampling) * ((f_sampling/2) - 3400);
 OUTPUT([1 : round(Ns), (length(OUTPUT) - round(Ns) + 1) : length(OUTPUT)]) = 0;
 out = real(ifft(ifftshift(OUTPUT)));
 filtered_output = out;
