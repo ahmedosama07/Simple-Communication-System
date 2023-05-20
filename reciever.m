b3d10794dc5c4f79890ae47f328d807a9deff24e
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-20 21:10:41 +0300

    Fixed a bug in reciever
    Receiver was working on mono channel only but now it works on mono and stereo hopefully

--- reciever.m
+++ reciever.m
@@ -6,7 +6,7 @@ function filtered_output = reciever(output, f_sampling)
 %   return: it returns the final output
 OUTPUT = fftshift(fft(output));
 Ns = (length(OUTPUT)/f_sampling) * ((f_sampling/2) - 3400);
-OUTPUT([1 : round(Ns), (length(OUTPUT) - round(Ns) + 1) : end]) = 0;
+OUTPUT([1 : round(Ns), (length(OUTPUT) - round(Ns) + 1) : end], :) = 0;
 out = real(ifft(ifftshift(OUTPUT)));
 filtered_output = out;
 end
